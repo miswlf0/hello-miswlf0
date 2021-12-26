b20dfc34ab55af8ba56ef1e61139b715c81545a4
miswlf0 <miswlf0@Hotmail.com>
2021-12-26 11:08:06 -0800

    patch for Lifu, he will have to do it later by himeself!

---
+++ multipler.c
@@ -0,0 +1,27 @@
+/*
+ * simple multipler function
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int mul(int x, int y) {
+
+	if (x == 0 || y == 0)
+		return 0;
+	return x * y;
+}
+
+int main(int argc, char **argv) {
+
+	int x,y;
+
+	if (argc < 3) {
+		printf("Insufficient args: Usage: a.out x y\n");
+		return -1;
+	}
+
+	x = atoi((const char *)argv[1]);
+	y = atoi((const char *)argv[2]);
+	printf("mul(%d, %d) = %d\n", x, y, mul(x,y));
+	return 0;
+}
