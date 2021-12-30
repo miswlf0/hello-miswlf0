f5f5d622e2d8cc79723f6165c872d618d02aa3af
Lifu MBP <Lifu.Wang@gmail.com>
2021-12-29 16:38:24 -0800

    multipler

---
+++ mul.c
@@ -0,0 +1,25 @@
+/*
+ * simple multipler 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int mul(int x, int y) {
+
+	return x * y;
+}
+
+int main(int argc, char **argv) {
+
+	int x,y;
+
+	if (argc < 3) {
+		printf("Insufficient args: Usage: a.out x y\n");
+		return -1;
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("mul(%d, %d) = %d\n", x, y, mul(x,y));
+	return 0;
+}
